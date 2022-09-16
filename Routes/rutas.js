070cdf5443dbac9c42950a5a007b1b97f41a0e44
204 <204@academicos.cesde.edu.co>
2022-09-16 08:37:47 -0500

    V1 apiNode

---
+++ Routes/rutas.js
@@ -0,0 +1,13 @@
+import  express from 'express'
+
+import { ControladorHabitacion } from '../Controllers/ControladorHabitacion.js'
+
+let controladorHabitacion = new ControladorHabitacion()//usando el controlador
+
+
+ export let rutasPersonalizadas = express.Router()
+
+rutasPersonalizadas.get('/hotelessanfe/habitaciones',controladorHabitacion.buscarHabitaciones)
+rutasPersonalizadas.get('/hotelessanfe/habitacion/:id',controladorHabitacion.buscarHabitacionPorId)
+rutasPersonalizadas.post('/hotelessanfe/habitacion',controladorHabitacion.registrarHabitacion)
+rutasPersonalizadas.put('/hotelessanfe/habitacion',controladorHabitacion.editarHabitacion)
