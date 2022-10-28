43930ee19cdebd7836928390b74a2a849583a976
yeseniamontoya <yeseniamontoya1036@gmail.com>
2022-10-28 08:08:13 -0500

    Se finaliza el servidor

--- Controllers/ControladorReservas.js
+++ Controllers/ControladorReservas.js
@@ -42,45 +42,91 @@ export class ControladorReservas{
 
         }
     }
-
-    async registrarReservas(request,response){
-        let datosReserva = request.body
-        let objetoServicioReserva= new ServicioReserva()
-
+    async registrarReservas(req, res){
+        let datosReserva = req.body
+        //console.log(datosReserva)
+        let objetoServicioReserva = new ServicioReserva()
+        let objetoServicioHabitacion = new ServicioHabitacion()
         try {
-            await objetoServicioReserva.agregarReservaEnBD(datosReserva)
-            response.status(200).json({
-                "mensaje":"exito registrando la reserva",
-                "datos":datosReserva,
-            })
-
+            let datosHabitacion = await objetoServicioHabitacion.buscarHabitacionPorId(datosReserva.id)
+            let maximoDePersonas = datosHabitacion.numeroMaximoPersonas
+            let totalPersonas = datosReserva.numeroDeNiños + datosReserva.numeroDeAdultos
+            let fechaDeIngreso = new Date(datosReserva.fechaDeEntrada)
+            let fechaDeExtraccion = new Date(datosReserva.fechaDeSalida)
+            const diffInDays = Math.floor((fechaDeExtraccion - fechaDeIngreso) / (1000 * 60 * 60 * 24))
+            let costo = 0
+            if (diffInDays > 0) {
+                if (maximoDePersonas >= totalPersonas) {
+                    console.log(datosHabitacion.valorNoche, diffInDays)
+                    costo = Number(datosHabitacion.valorNoche) * Number(diffInDays)
+                    datosReserva.costoDeReserva = costo
+                    console.log(datosReserva)
+                    await objetoServicioReserva.agregarReservaEnBD(datosReserva)
+                    res.status(200).json({
+                        "mensaje" :"Reserva exitosa",
+                        "datos":datosReserva,
+                    })
+                }else{
+                    response.status(400).json({
+                        "mensaje" : "La habitacion no esta capacitado para reservar a mas de " + maximoDePersonas,
+                        "datos" : null,
+                    })
+                }
+            }else{
+                response.status(400).json({
+                    "mensaje" : "Debes dijitar una fecha valida, como minimo debes quedarte un día",
+                    "datos" : null,
+                })
+            }
         } catch (error) {
-            response.status(400).json({
-                "mensaje":"error agregando "+error,
-                "datos":null,
+            res.status(400).json({
+                "mensaje" :"Error en el guardado " + error,
+                "datos":null
             })
-
         }
     }
 
-    async editarReservas(request,response){
-        let id = request.params.id
-        let datosReserva = request.body
-        let objetoServicioReserva= new ServicioReserva()
-
+    async editarReservas(req, res){
+        let id = req.params.idReserva
+        let datosReserva = req.body
+        let objetoServicioReserva = new ServicioReserva()
+        let objetoServicioHabitacion = new ServicioHabitacion()
         try {
-            await objetoServicioReserva.editarReserva(id,datosReserva)
-            response.status(200).json({
-                "mensaje":"exito editando la reserva"+id,
-                "datos":datosReserva,
-            })
-
+            let datosHabitacion = await objetoServicioHabitacion.buscarHabitacionPorId(datosReserva.id)
+            let maximoDePersonas = datosHabitacion.numeroMaximoPersonas
+            let totalPersonas = datosReserva.numeroDeNiños + datosReserva.numeroDeAdultos
+            let fechaDeIngreso = new Date(datosReserva.fechaDeEntrada)
+            let fechaDeExtraccion = new Date(datosReserva.fechaDeSalida)
+            const diffInDays = Math.floor((fechaDeExtraccion - fechaDeIngreso) / (1000 * 60 * 60 * 24))
+            let costo = 0
+            if (diffInDays > 0) {
+                if (maximoDePersonas >= totalPersonas) {
+                    console.log(datosHabitacion.valorNoche, diffInDays)
+                    costo = Number(datosHabitacion.valorNoche) * Number(diffInDays)
+                    datosReserva.costoDeReserva = costo
+                    console.log(datosReserva)
+                    await objetoServicioReserva.agregarReservaEnBD(datosReserva)
+                    res.status(200).json({
+                        "mensaje" :"Reserva exitosa",
+                        "datos":datosReserva,
+                    })
+                }else{
+                    response.status(400).json({
+                        "mensaje" : "La habitacion no esta capacitado para reservar a mas de " + maximoDePersonas,
+                        "datos" : null,
+                    })
+                }
+            }else{
+                response.status(400).json({
+                    "mensaje" : "Debes dijitar una fecha valida, como minimo debes quedarte un día",
+                    "datos" : null,
+                })
+            }
         } catch (error) {
-            response.status(400).json({
-                "mensaje":"error al editar "+error,
-                "datos":null,
+            res.status(400).json({
+                "mensaje" :"Error en el guardado " + error,
+                "datos":null
             })
-
         }
     }
 }
